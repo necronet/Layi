dd1f99f647d6fdfb4692e7060efc7197c5379398
Jose Ayerdis <jose.ayerdis@booking.com>
2013-06-06 00:26:46 +0200

    Add texthighlight and overlaying remove after first time

--- src/com/pozool/overla/OverlayComponent.java
+++ src/com/pozool/overla/OverlayComponent.java
@@ -2,7 +2,7 @@ package com.pozool.overla;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
+import android.content.SharedPreferences;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 public class OverlayComponent extends FrameLayout{
 
+	private String seed = "overlay";
 	private boolean hasCloseButton;
 	private OnClickListener closeCallback;
 	/**
@@ -60,6 +61,9 @@ public class OverlayComponent extends FrameLayout{
 	 * This will overlay a black background to the activity with a callback
 	 * */
 	public void wrap(Activity activity, OverlayCallback callback) {
+		
+		if(!isValid()) return;
+		
 		if (getParent() != null) {
 			Log.d("OverlayComponent", "parent is not null");
 			throw new IllegalStateException("There is already a overlay tutorial wrapping the activity");
@@ -89,6 +93,30 @@ public class OverlayComponent extends FrameLayout{
 		Log.d("OverlayComponent", "root was added");
 	}
 	
+	public void show() {
+
+		if(isValid())
+			setVisibility(View.VISIBLE);
+
+		SharedPreferences preferences = getContext().getSharedPreferences("OVERLAY_PREFS", Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = preferences.edit();
+		//increase by one the time shown
+		editor.putInt(seed, preferences.getInt(seed, 0) + 1);
+		editor.commit();
+	}
+	
+	
+	/** Check wether the overlay is valid to be shown. This is when it has not exceeded the time to be shown*/
+	public boolean isValid(){
+		SharedPreferences preferences = getContext().getSharedPreferences("OVERLAY_PREFS", Context.MODE_PRIVATE);
+		
+		int timeShown = preferences.getInt(seed, 0);
+		int frequency = preferences.getInt("frequency"+seed, 0);
+		
+		return (timeShown <= frequency);
+		
+	}
+	
 	static class OverlayClickListener implements OnClickListener {
 		
 		private OverlayCallback callback;
@@ -115,19 +143,39 @@ public class OverlayComponent extends FrameLayout{
 	
 	public void setHighlightText(String highLightText) {
 		int padding = getResources().getDimensionPixelOffset(R.dimen.padding_highlight);
-		TextView textView = new TextView(getContext());
-		textView.setText(highLightText);
-		textView.setTextSize(getResources().getDimension(R.dimen.text_size));
-		textView.setTextColor(getResources().getColor(android.R.color.white));
-		textView.setGravity(Gravity.CENTER);
-		textView.setPadding(padding, padding, padding, padding);
-		textView.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.figertap, 0, 0);
 		
+		View view = findViewById(R.id.text_highlight);
+		TextView textView = null;
+		if (view == null) {
+			textView =  new TextView(getContext());
+			textView.setId(R.id.text_highlight);
+			textView.setTextSize(getResources().getDimension(R.dimen.text_size));
+			textView.setTextColor(getResources().getColor(android.R.color.white));
+			textView.setGravity(Gravity.CENTER);
+			textView.setPadding(padding, padding, padding, padding);
+			textView.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.figertap, 0, 0);
+		} else {
+			textView = (TextView)view;
+		}
+
+		textView.setText(highLightText);
 		LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		params.gravity = Gravity.CENTER;
-		this.addView(textView, params);
+		
+		if (view == null)
+			addView(textView, params);
 	}
 	
+	public void showFrequency(int frequency) {
+		SharedPreferences preferences = getContext().getSharedPreferences("OVERLAY_PREFS", Context.MODE_PRIVATE);
+		if ( preferences.getInt("frequency"+seed, 1) != frequency) {
+			SharedPreferences.Editor editor = preferences.edit();
+			editor.putInt("frequency", frequency);
+			editor.commit();
+		}
+		
+	}
+
 	interface OverlayCallback extends OnClickListener{}
-	
+
 }
